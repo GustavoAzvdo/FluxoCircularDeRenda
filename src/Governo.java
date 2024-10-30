da3ce22bc6fc726edcf9301f2bfab0c5d6615322
Gustavo Azevedo <ghca12@gmail.com>
2024-10-30 16:08:47 -0300

    Atualizacao Governo

--- src/Governo.java
+++ src/Governo.java
@@ -5,4 +5,28 @@ public class Governo {
     public JPanel getTelaGoverno(){
         return telaGoverno;
     }
+    //Imposto
+    private JTextField txtImpRenda;
+    private JTextField txtImpCorporativo;
+    private JButton btnCalculaImp;
+
+    //Gastos Publicos
+    private JTextField txtSaude;
+    private JTextField txtEducacao;
+    private JTextField txtInfra;
+    private JTextField txtTotGastos;
+    private JButton btnCalculaGastos;
+
+    //Transferencias Sociais
+    private JTextField txtAssSocial;
+    private JTextField txtDesemprego;
+    private JTextField txtTotalSoc;
+    private JButton btnCalcularTrans;
+
+    //Saldos Orçamentarios
+    private JTextField txtGG;
+    private JTextField txtCofre;
+    private JTextField txtSituacao;
+    private JButton btnAnalisaSit;
+
 }
