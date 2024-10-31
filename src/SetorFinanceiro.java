ed833831b5e3d9a369895c800fbbf9a6e1acffbe
Jhonathan W. <jhonathanwilliam400@gmail.com>
2024-10-30 23:25:01 -0300

    adding a new screen
    I created the screen to see the balance and statement query

--- src/SetorFinanceiro.java
+++ src/SetorFinanceiro.java
@@ -2,14 +2,15 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import SetorFinanceiroScreens.CaptacaoRecursos;
-import SetorFinanceiroScreens.Investimento;
+import SetorFinanceiroScreens.*;
 public class SetorFinanceiro {
     private CaptacaoRecursos captacao = new CaptacaoRecursos();
     private Investimento investimento = new Investimento();
+    private Consulta consulta = new Consulta();
     private JPanel telaSetorFinanceiro;
     private JButton captacaoRecursosButton = new JButton("Captação de Recursos");
     private JButton investimentoButton = new JButton("Investir");
+    private JButton consultaSeEButton = new JButton("Consultar saldo e extrato");
 
     public SetorFinanceiro() {
         captacaoRecursosButton.addActionListener(new ActionListener() {
@@ -23,6 +24,13 @@ public class SetorFinanceiro {
             @Override
             public void actionPerformed(ActionEvent e) {
                 investimento.getTelaInvestimento();
+            }
+        });
+
+        consultaSeEButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                consulta.getTelaConsulta();
             }
         });
     }
---
+++ src/SetorFinanceiroScreens/Consulta.java
@@ -0,0 +1,11 @@
+package SetorFinanceiroScreens;
+
+import javax.swing.JPanel;
+
+public class Consulta {
+    private JPanel telaConsulta;
+
+    public JPanel getTelaConsulta(){
+        return telaConsulta;
+    }
+}
