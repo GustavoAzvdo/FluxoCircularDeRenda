561abf26f0cca2d9abd4ce697afbce111832d6a1
Matheus <matheuspolipo@gmail.com>
2024-11-01 21:34:37 -0300

    Atualizacao Empresa e Familia

--- src/Empresa.java
+++ src/Empresa.java
@@ -2,6 +2,12 @@ import javax.swing.*;
 
 public class Empresa {
     private JPanel telaEmpresa;
+    private JButton btnPagarImposto;
+    private JTextField txtSalario;
+    private JTextField txtContasPendentes;
+    private JButton btnRegistrarCusto;
+    private JTextField txtMercadoriaVender;
+    private JButton btnVenderMercadoria;
 
     public JPanel getTelaEmpresa(){
         return telaEmpresa;
